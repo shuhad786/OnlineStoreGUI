fc21d28cd482442d99fafbde743603eec9a5dbac
Shuhad786 <65858136+Shuhad786@users.noreply.github.com>
2021-12-02 14:48:43 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿
+namespace OnlineStoreGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Inventory_listBox = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CheckOut_dataGridView = new System.Windows.Forms.DataGridView();
+            this.label9 = new System.Windows.Forms.Label();
+            this.First_textBox = new System.Windows.Forms.TextBox();
+            this.Connect_dataGridView = new System.Windows.Forms.DataGridView();
+            this.CheckOut_listBox = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Connect_label = new System.Windows.Forms.Label();
+            this.CheckOut_label = new System.Windows.Forms.Label();
+            this.Add_label = new System.Windows.Forms.Label();
+            this.Connect_button = new System.Windows.Forms.Button();
+            this.CheckOut_button = new System.Windows.Forms.Button();
+            this.Add_button = new System.Windows.Forms.Button();
+            this.Fourth_textBox = new System.Windows.Forms.TextBox();
+            this.Third_textBox = new System.Windows.Forms.TextBox();
+            this.Second_textBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CheckOut_dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Connect_dataGridView)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Inventory_listBox
+            // 
+            this.Inventory_listBox.FormattingEnabled = true;
+            this.Inventory_listBox.ItemHeight = 15;
+            this.Inventory_listBox.Location = new System.Drawing.Point(328, 3);
+            this.Inventory_listBox.Name = "Inventory_listBox";
+            this.Inventory_listBox.Size = new System.Drawing.Size(589, 124);
+            this.Inventory_listBox.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.CheckOut_dataGridView);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.First_textBox);
+            this.panel1.Controls.Add(this.Connect_dataGridView);
+            this.panel1.Controls.Add(this.CheckOut_listBox);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.Fourth_textBox);
+            this.panel1.Controls.Add(this.Third_textBox);
+            this.panel1.Controls.Add(this.Second_textBox);
+            this.panel1.Controls.Add(this.Inventory_listBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(920, 524);
+            this.panel1.TabIndex = 2;
+            // 
+            // CheckOut_dataGridView
+            // 
+            this.CheckOut_dataGridView.AllowUserToOrderColumns = true;
+            this.CheckOut_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CheckOut_dataGridView.Location = new System.Drawing.Point(328, 395);
+            this.CheckOut_dataGridView.Name = "CheckOut_dataGridView";
+            this.CheckOut_dataGridView.RowTemplate.Height = 25;
+            this.CheckOut_dataGridView.Size = new System.Drawing.Size(589, 126);
+            this.CheckOut_dataGridView.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(113, 234);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 15);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Control Menu";
+            // 
+            // First_textBox
+            // 
+            this.First_textBox.Location = new System.Drawing.Point(92, 12);
+            this.First_textBox.Name = "First_textBox";
+            this.First_textBox.Size = new System.Drawing.Size(155, 23);
+            this.First_textBox.TabIndex = 18;
+            this.First_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.First_textBox_KeyPress);
+            // 
+            // Connect_dataGridView
+            // 
+            this.Connect_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Connect_dataGridView.Location = new System.Drawing.Point(328, 263);
+            this.Connect_dataGridView.Name = "Connect_dataGridView";
+            this.Connect_dataGridView.RowTemplate.Height = 25;
+            this.Connect_dataGridView.Size = new System.Drawing.Size(589, 126);
+            this.Connect_dataGridView.TabIndex = 3;
+            // 
+            // CheckOut_listBox
+            // 
+            this.CheckOut_listBox.FormattingEnabled = true;
+            this.CheckOut_listBox.ItemHeight = 15;
+            this.CheckOut_listBox.Location = new System.Drawing.Point(328, 138);
+            this.CheckOut_listBox.Name = "CheckOut_listBox";
+            this.CheckOut_listBox.Size = new System.Drawing.Size(589, 124);
+            this.CheckOut_listBox.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(262, 129);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 15);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "E.g Phone";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(262, 91);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "E.g Nokia";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(262, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "E.g 56,7";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(262, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Amount";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Description:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Brand name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Price:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Quantity:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.Connect_label);
+            this.panel2.Controls.Add(this.CheckOut_label);
+            this.panel2.Controls.Add(this.Add_label);
+            this.panel2.Controls.Add(this.Connect_button);
+            this.panel2.Controls.Add(this.CheckOut_button);
+            this.panel2.Controls.Add(this.Add_button);
+            this.panel2.Location = new System.Drawing.Point(6, 263);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(319, 107);
+            this.panel2.TabIndex = 3;
+            // 
+            // Connect_label
+            // 
+            this.Connect_label.AutoSize = true;
+            this.Connect_label.Location = new System.Drawing.Point(123, 89);
+            this.Connect_label.Name = "Connect_label";
+            this.Connect_label.Size = new System.Drawing.Size(86, 15);
+            this.Connect_label.TabIndex = 5;
+            this.Connect_label.Text = "Not connected";
+            // 
+            // CheckOut_label
+            // 
+            this.CheckOut_label.AutoSize = true;
+            this.CheckOut_label.Location = new System.Drawing.Point(123, 50);
+            this.CheckOut_label.Name = "CheckOut_label";
+            this.CheckOut_label.Size = new System.Drawing.Size(63, 15);
+            this.CheckOut_label.TabIndex = 4;
+            this.CheckOut_label.Text = "Total R0.00";
+            // 
+            // Add_label
+            // 
+            this.Add_label.AutoSize = true;
+            this.Add_label.Location = new System.Drawing.Point(123, 11);
+            this.Add_label.Name = "Add_label";
+            this.Add_label.Size = new System.Drawing.Size(87, 15);
+            this.Add_label.TabIndex = 3;
+            this.Add_label.Text = "Nothing added";
+            // 
+            // Connect_button
+            // 
+            this.Connect_button.Location = new System.Drawing.Point(3, 81);
+            this.Connect_button.Name = "Connect_button";
+            this.Connect_button.Size = new System.Drawing.Size(104, 23);
+            this.Connect_button.TabIndex = 2;
+            this.Connect_button.Text = "Update database";
+            this.Connect_button.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Connect_button.UseVisualStyleBackColor = true;
+            this.Connect_button.Click += new System.EventHandler(this.Connect_button_Click);
+            // 
+            // CheckOut_button
+            // 
+            this.CheckOut_button.Location = new System.Drawing.Point(3, 42);
+            this.CheckOut_button.Name = "CheckOut_button";
+            this.CheckOut_button.Size = new System.Drawing.Size(75, 23);
+            this.CheckOut_button.TabIndex = 1;
+            this.CheckOut_button.Text = "Check Out";
+            this.CheckOut_button.UseVisualStyleBackColor = true;
+            this.CheckOut_button.Click += new System.EventHandler(this.CheckOut_button_Click);
+            // 
+            // Add_button
+            // 
+            this.Add_button.Location = new System.Drawing.Point(3, 3);
+            this.Add_button.Name = "Add_button";
+            this.Add_button.Size = new System.Drawing.Size(75, 23);
+            this.Add_button.TabIndex = 0;
+            this.Add_button.Text = "Add";
+            this.Add_button.UseVisualStyleBackColor = true;
+            this.Add_button.Click += new System.EventHandler(this.Add_button_Click);
+            // 
+            // Fourth_textBox
+            // 
+            this.Fourth_textBox.Location = new System.Drawing.Point(92, 125);
+            this.Fourth_textBox.Name = "Fourth_textBox";
+            this.Fourth_textBox.Size = new System.Drawing.Size(155, 23);
+            this.Fourth_textBox.TabIndex = 5;
+            this.Fourth_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Fourth_textBox_KeyPress);
+            // 
+            // Third_textBox
+            // 
+            this.Third_textBox.Location = new System.Drawing.Point(92, 87);
+            this.Third_textBox.Name = "Third_textBox";
+            this.Third_textBox.Size = new System.Drawing.Size(155, 23);
+            this.Third_textBox.TabIndex = 4;
+            this.Third_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Third_textBox_KeyPress);
+            // 
+            // Second_textBox
+            // 
+            this.Second_textBox.Location = new System.Drawing.Point(92, 49);
+            this.Second_textBox.Name = "Second_textBox";
+            this.Second_textBox.Size = new System.Drawing.Size(155, 23);
+            this.Second_textBox.TabIndex = 3;
+            this.Second_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Second_textBox_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 548);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CheckOut_dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Connect_dataGridView)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox Inventory_listBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button Connect_button;
+        private System.Windows.Forms.Button CheckOut_button;
+        private System.Windows.Forms.Button Add_button;
+        private System.Windows.Forms.TextBox Fourth_textBox;
+        private System.Windows.Forms.TextBox Third_textBox;
+        private System.Windows.Forms.TextBox Second_textBox;
+        private System.Windows.Forms.Label Connect_label;
+        private System.Windows.Forms.Label CheckOut_label;
+        private System.Windows.Forms.Label Add_label;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox CheckOut_listBox;
+        private System.Windows.Forms.DataGridView Connect_dataGridView;
+        private System.Windows.Forms.TextBox First_textBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridView CheckOut_dataGridView;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using OnlineStoreLibrary;
+using System.Data.SqlClient;
+
+namespace OnlineStoreGUI
+{
+    public partial class Form1 : Form
+    {
+        BindingSource InputSource = new();
+        BindingSource OutPutSource = new();
+        OnlineStore OnlineAccess = new();
+        SqlConnection CN = new(@"Data Source=DESKTOP-LAIUVSI\SQLEXPRESS;Initial Catalog=Inventory;Integrated Security=True");
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Adding a product via button click
+        private void Add_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Adds values from text box to the list 
+                Products Input = new(int.Parse(First_textBox.Text), decimal.Parse(Second_textBox.Text), (Third_textBox.Text), (Fourth_textBox.Text));
+                OnlineAccess.ProductsList.Add(Input);
+                First_textBox.Text = "";
+                Second_textBox.Text = "";
+                Third_textBox.Text = "";
+                Fourth_textBox.Text = "";
+                Add_label.Text = "Added new product";
+                Add_label.ForeColor = Color.Blue;
+                InputSource.ResetBindings(false); // resets the list box
+                Connect_label.Text = "Connected";
+                Connect_label.ForeColor = Color.Green;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // to display product to check out display box
+        private void CheckOut_button_Click(object sender, EventArgs e)
+        {
+            Products added = (Products)Inventory_listBox.SelectedItem;
+            OnlineAccess.OnlineStoreList.Add(added);
+            decimal total = OnlineAccess.CheckOut();
+            CheckOut_label.Text = "R" + total.ToString();
+            OutPutSource.ResetBindings(false);
+            OnlineAccess.PrintReceipt(); // prints a text file to path file in OnlineStore class
+
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Added new product display
+            InputSource.DataSource = OnlineAccess.ProductsList;
+            Inventory_listBox.DataSource = InputSource;
+            Inventory_listBox.DisplayMember = ToString();
+            
+
+
+            // Check out product display to box
+            
+            OutPutSource.DataSource = OnlineAccess.OnlineStoreList;
+            CheckOut_listBox.DataSource = OutPutSource;
+            CheckOut_listBox.DisplayMember = ToString();
+           
+
+            
+            Connect_label.ForeColor = Color.Red;
+        }
+
+        private void Connect_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+
+                CN.Open();
+                string selected1 = Inventory_listBox.SelectedItem.ToString();
+                string q = "select * from Input_table where Brand='" + selected1 + "'";
+               
+                SqlDataAdapter SDA1 = new(q, CN);
+                DataTable DT1 = new();
+                SDA1.Fill(DT1);
+                Connect_dataGridView.DataSource = InputSource;
+                InputSource.ResetBindings(false);
+
+                string selected2 = CheckOut_listBox.SelectedItem.ToString();
+                string r = "select * from Input_table where Brand='" + selected2 + "'";
+                SqlDataAdapter SDA2 = new(r, CN);
+                DataTable DT2 = new();
+                SDA2.Fill(DT2);
+                CheckOut_dataGridView.DataSource = OutPutSource;
+                OutPutSource.ResetBindings(false);
+
+                Connect_label.Text = "Updated";
+                Connect_label.ForeColor = Color.Goldenrod;
+
+                CN.Close(); // closes connection
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+            }
+            // Only allows numbers to be entered in First_textBox
+            private void First_textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        // Only allows numbers to be entered in Second_textBox
+        private void Second_textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        // Only allows Letters to be entered in Third_textBox
+        private void Third_textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+        
+        // Only allows Letters to be entered in Fourth_textBox
+        private void Fourth_textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+
+      
+
+       
+    }
+}
